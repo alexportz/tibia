a24a98d09db5e5e6b610a3082b1e6f3b99969c44
Alex Portz <alex.portz@live.com>
2024-05-04 02:12:28 -0300

    Desenvolvido sistema Responsivo da ViewPort

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Projeto Tibia no Browser
-Este é um Projeto Open Source de desenvolvimento de um mini Tibia via Browser com Javascript, Html e CSS.
+
+Este é um Projeto Open Source de desenvolvimento de um mini Tibia via Browser com HTML/Canvas & Javascript.
 Para estudos e treino de skills, de pessoas iniciantes em Javascript.
+
+Projeto Iniciado por
+#AlexPortz
--- js/Mapa.js
+++ js/Mapa.js
@@ -1,24 +1,37 @@
 export default class Mapa {
-    constructor(ctx,x,y,w,h,sqmSizeX,sqmSizeY,maxSqmX,maxSqmY,borderColor){
+    constructor(ctx,x,y,w,h,sqmSizeX,sqmSizeY,maxSqmX,maxSqmY,borderColor,screenSizeX,screenSizeY){
         this.ctx = ctx;
         this.borderColor = borderColor;
         this.sqmSizeX = sqmSizeX;
         this.sqmSizeY = sqmSizeY;
         this.maxSqmX = maxSqmX;
         this.maxSqmY = maxSqmY;
+        this.screenSize = {
+            w: screenSizeX,
+            h: screenSizeY
+        }
         this.position = {
-            x: (innerWidth/2) - ((this.sqmSizeX * this.maxSqmX) / 2),
-            y: y
+            x: (this.screenSize.w/2) - ((this.sqmSizeX * this.maxSqmX) / 2),
+            y: (this.screenSize.h/2) - ((this.sqmSizeY * this.maxSqmY) / 2)
         };
         this.size = {
             w: this.sqmSizeX * this.maxSqmX,
             h: this.sqmSizeY * this.maxSqmY
-        }
+        };
+
     }
 
     //Função Desenha Borda
     draw(){
         this.ctx.strokeStyle = this.borderColor;
         this.ctx.strokeRect(this.position.x,this.position.y,this.size.w,this.size.h);
+    }
+
+    //Função atualiza Posição
+    attPosition(){
+        this.position = {
+        x: (this.screenSize.w/2) - ((this.sqmSizeX * this.maxSqmX) / 2),
+        y: (this.screenSize.h/2) - ((this.sqmSizeY * this.maxSqmY) / 2)
+        }
     }
 }
